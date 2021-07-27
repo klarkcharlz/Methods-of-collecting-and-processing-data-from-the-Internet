ba2996776cd51fb2cb77cc131b5b28e8fa8a1c30
klarkcharlz <klark.charlz@mail.ru>
2021-07-27 17:45:34 +0300

    [upd] исправленная версия

--- func.py
+++ func.py
@@ -4,22 +4,22 @@ def pars_salary(salary: str) -> tuple:
         if salary:
             temp1 = salary.split(" – ")
             if len(temp1) == 2:
-                min_salary = temp1[0]
-                temp2 = temp1[1].split(" ")
-                max_salary = f"{temp2[0]} {temp2[1]}"
-                currency = temp2[2]
+                min_salary = " ".join(temp1[0].split("\u202f"))
+                temp2 = temp1[1].split("\u202f")
+                max_salary = f"{temp2[0]} {temp2[1].split(' ')[0]}"
+                currency = temp2[1].split(' ')[1]
                 return min_salary, max_salary, currency
             if len(temp1) == 1:
-                temp2 = temp1[1].split(" ")
+                temp2 = temp1[0].split(" ")
                 if temp2[0] == "до":
                     min_salary = None
-                    max_salary = f"{temp2[1]} {temp2[2]}"
-                    currency = temp2[3]
+                    max_salary = " ".join(temp2[1].split("\u202f"))
+                    currency = temp2[2]
                 elif temp2[0] == "от":
-                    min_salary = f"{temp2[1]} {temp2[2]}"
+                    min_salary = " ".join(temp2[1].split("\u202f"))
                     max_salary = None
-                    currency = temp2[3]
+                    currency = temp2[2]
                 return min_salary, max_salary, currency
     except Exception as err:
-        print(salary)
+        print(f"{type(err)}:\n{err}")
     return None, None, None
