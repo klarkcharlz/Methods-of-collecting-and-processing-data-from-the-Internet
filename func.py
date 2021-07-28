9e213e1682900c0ca696529eb0b5ac6a3861beb1
klarkcharlz <klark.charlz@mail.ru>
2021-07-28 20:06:59 +0300

    [upd] 2 convert salary to int

--- func.py
+++ func.py
@@ -4,19 +4,19 @@ def pars_salary(salary: str) -> tuple:
         if salary:
             temp1 = salary.split(" – ")
             if len(temp1) == 2:
-                min_salary = " ".join(temp1[0].split("\u202f"))
+                min_salary = "".join(temp1[0].split("\u202f"))
                 temp2 = temp1[1].split("\u202f")
-                max_salary = f"{temp2[0]} {temp2[1].split(' ')[0]}"
+                max_salary = f"{temp2[0]}{temp2[1].split(' ')[0]}"
                 currency = temp2[1].split(' ')[1]
-                return min_salary, max_salary, currency
+                return int(min_salary), int(max_salary), currency
             if len(temp1) == 1:
                 temp2 = temp1[0].split(" ")
                 if temp2[0] == "до":
                     min_salary = None
-                    max_salary = " ".join(temp2[1].split("\u202f"))
+                    max_salary = int("".join(temp2[1].split("\u202f")))
                     currency = temp2[2]
                 elif temp2[0] == "от":
-                    min_salary = " ".join(temp2[1].split("\u202f"))
+                    min_salary = int("".join(temp2[1].split("\u202f")))
                     max_salary = None
                     currency = temp2[2]
                 return min_salary, max_salary, currency
